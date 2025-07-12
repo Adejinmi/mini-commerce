34d01ddf304be3404cb499d3163ae79547544ad8
Samuel <adejinmisamuel1@gmail.com>
2025-07-12 08:37:41 +0100

    Added 1s delay to product fetching to imitate network delay

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -2,6 +2,7 @@ import products from "@/data/products.json";
 import type { Product } from "@/components/ProductCard";
 
 export const fetchProducts = async (): Promise<Product[]> => {
-  await new Promise((res) => setTimeout(res, 300));
+  //1s delay to imitate network delay
+  await new Promise((res) => setTimeout(res, 1000));
   return products;
 };
