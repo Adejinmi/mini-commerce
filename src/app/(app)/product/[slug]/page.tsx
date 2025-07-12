79bc27cbc76923bd3e7faf936ba682c208326e35
Samuel <adejinmisamuel1@gmail.com>
2025-07-12 20:06:53 +0100

    Added 'Go back' button to product details page

--- src/app/(app)/product/[slug]/page.tsx
+++ src/app/(app)/product/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { notFound } from "next/navigation";
 import { fetchProducts } from "@/lib/fetcher";
 import ProductDetail from "@/components/productDetails";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 type Params = { params: { slug: string } };
 
@@ -14,5 +17,15 @@ export default async function ProductPage({ params }: Params) {
 
   if (!product) return notFound();
 
-  return <ProductDetail product={product} alsoBought={alsoBought} />;
+  return (
+    <>
+      <Button variant={"link"} asChild className="md:mx-[50px] p-6">
+        <Link href={"/"}>
+          <ArrowLeft />
+          Go back
+        </Link>
+      </Button>
+      <ProductDetail product={product} alsoBought={alsoBought} />;
+    </>
+  );
 }
